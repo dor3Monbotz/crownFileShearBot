13d5c40bdfbd0d2cbe2acea0b0355f5844db0ba7
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2022-11-27 09:16:31 +0530

    Add files via upload

---
+++ plugins/route.py
@@ -0,0 +1,13 @@
+#(©)crownbotz
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("ᴄʀᴏᴡɴʙᴏᴛᴢ")
